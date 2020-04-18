ce4d96bd85fee6eafee009ab8872a378da794709
kifack <kifack.yanick@gmail.com>
2020-04-18 15:19:35 +0200

    my first commit

---
+++ readme.md
@@ -0,0 +1,7 @@
+Application Apangolin
+
+L'objectif est de créer un mini carnet d'adresse pour "Pangolin" sur Express.js (sous forme d'API) avec un front sur angular &  DB Mongo. 
+
+(Inscription/Connexion/Déconnexion) du "Pangolin" par login/mot de passe 
+(Afficher/Modifier) ses informations (age / famille / race / nourriture) 
+(Ajouter/Supprimer) en amis un autre "Pangolin" parmi les autres Pangolins inscrits.
---
+++ routes/user.route.js
@@ -0,0 +1,106 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const config = require('../config/database');
+const User = require('../models/user.model');
+const Friend = require('../models/friend.model');
+
+
+router.post('/register', (req, res, next) => {
+  let newUser = new User ({
+    email: req.body.email,
+    name: req.body.name,
+    password: req.body.password
+  });
+
+
+  User.addUser(newUser, (err, user) => {
+    if(err) {
+      res.json({success: false, msg: "Erreur lors de la creation du compte"});
+    } else {
+      res.json({success: true, msg: 'Utilisateur inscrit'});
+    }
+  });
+});
+
+router.put('/update-profile', (req, res, next) => {
+ 
+  User.findOneAndUpdate({_id:req.body._id},{
+       name:req.body.name,
+       email:req.body.email,
+       family:req.body.family,
+       race:req.body.race,
+       food:req.body.food,
+       age:req.body.age
+   }).then((err,user) => {
+     
+     res.json({success: true, msg: "Compte modifié"});
+   });
+});
+
+
+router.post('/authenticate', (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.getUserByEmail(email, (err, user) => {
+    if(err) throw err;
+    if(!user) {
+      return res.json({success: false, msg: 'Utilisateur introuvable'});
+    }
+
+    User.comparePassword(password, user.password, (err, isMatch) => {
+      if(err) throw err;
+      if(isMatch) {
+        const token = jwt.sign({data: user}, config.secret, {
+          expiresIn: 604800 
+        });
+        res.json({
+          success: true,
+          token: 'JWT '+token,
+          user: {
+            id: user._id,
+            name: user.name,
+            email: user.email
+          }
+        })
+      } else {
+        return res.json({success: false, msg: 'Mauvais mot de passe'});
+      }
+    });
+  });
+});
+
+
+router.get('/profile', passport.authenticate('jwt', {session:false}), (req, res, next) => {
+  res.json({user: req.user});
+});
+
+router.get('/all-users', passport.authenticate('jwt', {session:false}), (req, res, next) => {
+
+ 
+ Friend.find({user:req.user._id }, function(err, users){
+    var userIDs;
+    if (err) {
+      return;
+    }
+
+    userIDs = users.map(function (user) { return user.friend; });
+
+    User.find({_id: {$nin: userIDs}}, function (err, items) {
+        if (err) {
+          return;
+        }
+           
+           res.json({success: true, users: items,user: req.user})
+  });
+
+});
+
+});
+
+
+
+
+module.exports = router;
